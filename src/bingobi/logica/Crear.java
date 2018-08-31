a9777248e16eae837d23361056916ce66496155f
alexkbra <alexkbraloco26@gmail.com>
2018-08-31 18:50:54 -0500

    programacion 2

--- src/bingobi/logica/Crear.java
+++ src/bingobi/logica/Crear.java
@@ -6,6 +6,7 @@
 package bingobi.logica;
 
 import bingobi.logica.bean.BingoBean;
+import bingobi.logica.util.DateUtil;
 import bingobi.persistence.controllers.CartonesJpaController;
 import bingobi.persistence.controllers.DiasJpaController;
 import bingobi.persistence.controllers.HorariosJpaController;
@@ -33,11 +34,13 @@ import javax.persistence.EntityManagerFactory;
  * @author kbra
  */
 public class Crear {
-    
-    public void crearTablasBingo(EntityManagerFactory emf){
-        CartonesJpaController cartoneController = new  CartonesJpaController(emf);
+
+    DateUtil dateUtil;
+
+    public void crearTablasBingo(EntityManagerFactory emf) {
+        CartonesJpaController cartoneController = new CartonesJpaController(emf);
         TablasJpaController tablaController = new TablasJpaController(emf);
-        Integer b=1,i=18,n=36,g=54,o=72;
+        Integer b = 1, i = 18, n = 36, g = 54, o = 72;
         BingoBean bean = new BingoBean(b, i, n, g, o);
         for (int j = 1; j <= 72; j++) {
             Tablas tabla = new Tablas();
@@ -47,51 +50,54 @@ public class Crear {
                 Cartones carton = new Cartones();
                 carton.setB(b.toString());
                 carton.setI(i.toString());
-                if(k == 2){
+                if (k == 2) {
                     carton.setN(" ");
-                }else{
+                } else {
                     carton.setN(n.toString());
                 }
                 carton.setG(g.toString());
                 carton.setO(o.toString());
                 carton.setIdTabla(tabla);
                 cartoneController.create(carton);
-                b++;i++;g++;o++;
-                if(k != 2){
+                b++;
+                i++;
+                g++;
+                o++;
+                if (k != 2) {
                     n++;
                 }
-                if(b > 17){
+                if (b > 17) {
                     b = 1;
                 }
-                if(i > 35){
+                if (i > 35) {
                     i = 18;
                 }
-                if(n > 53){
+                if (n > 53) {
                     n = 36;
                 }
-                if(g > 71){
+                if (g > 71) {
                     g = 54;
                 }
-                if(o > 90){
+                if (o > 90) {
                     o = 72;
                 }
             }
         }
     }
-    
-    public String[] getStrDays(){
+
+    public String[] getStrDays() {
         return new String[]{
-                        "7",//DOMINGO
-                        "1",//LUNES
-                        "2",//MARTES
-                        "3",//MARTES
-                        "4",//JUEVES
-                        "5",//VIERNES
-                        "6"//SÁBADO
-                    };
+            "7",//DOMINGO
+            "1",//LUNES
+            "2",//MARTES
+            "3",//MARTES
+            "4",//JUEVES
+            "5",//VIERNES
+            "6"//SÁBADO
+        };
     }
-    
-    public void crearProgramacion(EntityManagerFactory emf) throws Exception{
+
+    public void crearProgramacion(EntityManagerFactory emf) throws Exception {
         Scanner sc = new Scanner(System.in);
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
         SedesJpaController sedes = new SedesJpaController(emf);
@@ -100,113 +106,158 @@ public class Crear {
         ProgramacionesJpaController programaciones = new ProgramacionesJpaController(emf);
         DiasJpaController dias = new DiasJpaController(emf);
         String dateStart = "";
-        System.out.println("date init yyyy-MM-dd");
-        dateStart = sc.next();
-        
-        Date dateBegin = formatter.parse(dateStart);
+        Date today = new Date();
+        System.out.println("date init yyyy-MM-dd " + formatter.format(today));
+        //dateStart = sc.next();
+        Date dateBegin = today;
+        dateUtil = new DateUtil(dateBegin.getYear());
         int countMonths = 0, monthOld = dateBegin.getMonth(), monthNew = 0;
         Horarios lunes = horarios.findHorarios(1);
-        
         String[] strDays = getStrDays();
-        
-        while (countMonths < 5 ) {
-            List<Horarios> listHorarios = horarios.findHorariosEntities();
-            int countHorariosNormal = 1;
-            int countHorariosDJ = 0;//4h
-            int countHorariosVSD = 0;//2h
-            int counthorariosFinal = 0;
-            Random r = new Random();
-            Integer tablas = r.nextInt(72)+1;
-            double porcentajeVentas = tablas*0.4;
-            Calendar now = Calendar.getInstance();
-            for (Horarios horario : listHorarios) {
-                if(horario.getIdDia().getId() != lunes.getId()){
-                    String dia = strDays[now.get(Calendar.DAY_OF_WEEK) - 1];
-                    TipoBingos tipoBingo = null;
-                    //Todos los días, cada 30min y 60 min
-                    switch(countHorariosNormal){
-                        case 1:
-                            Sedes sede1 = sedes.findSedes(1);
-                            Sedes sede2 = sedes.findSedes(2);
-                            Sedes sede3 = sedes.findSedes(3);
-                            Sedes sede4 = sedes.findSedes(4);
-                            if (tablas > porcentajeVentas){
-                                tipoBingo = tipoBingoController.findTipoBingos(1);
-                                
-                            }else{
-                                if(tablas >= 10){
-                                    tipoBingo = tipoBingoController.findTipoBingos(7);
-                                }
-                            }
-                            break;
-                        case 2:
-                            
-                            break;
-                            
-                    }
-                    countHorariosNormal++;
-                    if (countHorariosNormal > 2)countHorariosNormal = 1;
-                    //Domingo y Jueves, 4 horas
-                    countHorariosDJ++;
-                    if(countHorariosDJ == 8){
-                        tipoBingo = tipoBingoController.findTipoBingos(3);
-                        countHorariosDJ = 0;
-                    }
-                    //Viernes, Sabado y Domingo, 2 Horas
-                    countHorariosVSD++;    
-                    if(countHorariosVSD == 4){
-                        tipoBingo = tipoBingoController.findTipoBingos(4);
-                        countHorariosVSD = 0;
-                    }
-                    //Final 
-                    counthorariosFinal++;
-                    if(counthorariosFinal == (listHorarios.size()-1)){
-                        tipoBingo = tipoBingoController.findTipoBingos(5);
-                        counthorariosFinal = 0;
-                    }
-                    //BINGAMES
-                    Date dateEnd =new Date((long) dateBegin.getTime()+(86400000));
-                    int monthNewEnd = dateEnd.getMonth();
-                    if( monthOld != monthNewEnd  ){
-                        tipoBingo = tipoBingoController.findTipoBingos(6);
-                        //Determinar el 
-                        
-                    }
-                    
-                }
-            }
-            dateBegin.setTime((long) dateBegin.getTime()+(86400000));
+        while (countMonths < 5) {
+
+            Sedes sede1 = sedes.findSedes(1);
+            crearProgramacion(lunes, strDays, dateBegin, monthOld, sede1, programaciones, horarios, tipoBingoController);
+            
+            Sedes sede2 = sedes.findSedes(2);
+            crearProgramacion(lunes, strDays, dateBegin, monthOld, sede2, programaciones, horarios, tipoBingoController);
+            
+            Sedes sede3 = sedes.findSedes(3);
+            crearProgramacion(lunes, strDays, dateBegin, monthOld, sede3, programaciones, horarios, tipoBingoController);
+            
+            Sedes sede4 = sedes.findSedes(4);
+            crearProgramacion(lunes, strDays, dateBegin, monthOld, sede4, programaciones, horarios, tipoBingoController);
+            
+            dateBegin.setTime((long) dateBegin.getTime() + (86400000));
             monthNew = dateBegin.getMonth();
-               
-            if( monthOld != monthNew  ){
+            if (monthOld != monthNew) {
                 countMonths++;
                 monthOld = dateBegin.getMonth();
             }
             System.out.println(formatter.format(dateBegin));
-            /*Programaciones programacion = new Programaciones();
-            
-            Integer idSede = r.nextInt(sedes.getSedesCount())+1;
-            Integer idHorarios = r.nextInt(horarios.getHorariosCount())+1;
-            Integer idTipoBing = r.nextInt(tipoBingo.getTipoBingosCount())+1;
-            
-            
-            TipoBingos tipoBingos = tipoBingo.findTipoBingos(idTipoBing);
-            programacion.setIdSede(sedes.findSedes(idSede));
-            programacion.setIdHorario(horarios.findHorarios(idHorarios));
-            programacion.setIdTipoBingo(tipoBingos);
-            programacion.setEstado(0);
-            programacion.setCantidadGanadores(String.valueOf(0));
-            programacion.setCantidadPremio(tipoBingos.getValorPremio());
-            programacion.setCantidadTablas(tablas);
-            programacion.setFecha(dateBegin);
-            
-            programaciones.create(programacion);*/
-            
-            
-            
-            
         }
     }
+
+    public void crearProgramacion(Horarios lunes, String[] strDays, Date dateBegin, int monthOld, Sedes sedes, ProgramacionesJpaController programaciones, HorariosJpaController horarios, TipoBingosJpaController tipoBingoController) {
+        List<Horarios> listHorarios = horarios.findHorariosEntities();
+        int countHorariosNormal = 1;
+        int countHorariosDJ = 0;//4h
+        int countHorariosVSD = 0;//2h
+        int counthorariosFinal = 0;
+        Random r = new Random();
+        Integer totalTablas = 72;
+        Integer tablasVentas = r.nextInt(totalTablas) + 1;
+        double porcentajeVentas = totalTablas * (0.4);
+        Calendar now = Calendar.getInstance();
+
+        for (Horarios horario : listHorarios) {
+            if (horario.getIdDia().getId() != lunes.getId()) {
+                String dia = strDays[now.get(Calendar.DAY_OF_WEEK) - 1];
+                TipoBingos tipoBingo = null;
+                //Todos los días, cada 30min y 60 min
+                switch (countHorariosNormal) {
+                    case 1:
+                        //BINGO(NORMAL)
+                        if (tablasVentas > porcentajeVentas) {
+                            tipoBingo = tipoBingoController.findTipoBingos(1);
+                            crearProgramacion(tipoBingo, horario, tablasVentas, programaciones, sedes, dateBegin);
+                        } else {
+                            //BINGITO
+                            tipoBingo = tipoBingoController.findTipoBingos(7);
+                            if (tablasVentas >= 10) {
+                                crearProgramacion(tipoBingo, horario, tablasVentas, programaciones, sedes, dateBegin);
+                            } else {
+                                crearProgramacion(tipoBingo, horario, tablasVentas,  programaciones, sedes, dateBegin,0);    
+                            }
+                        }
+                        break;
+                    case 2:
+                        //totalTablas
+                        tipoBingo = tipoBingoController.findTipoBingos(2);
+                        crearProgramacion(tipoBingo, horario, tablasVentas, programaciones, sedes, dateBegin);
+                        break;
+
+                }
+                //TODOS LOS DÏAS
+                countHorariosNormal++;
+                if (countHorariosNormal > 2) {
+                    countHorariosNormal = 1;
+                }
+
+                //Domingo y Jueves, 4 horas
+                //SUPER BINGO
+                countHorariosDJ++;
+                if (countHorariosDJ == 8) {
+                    tipoBingo = tipoBingoController.findTipoBingos(3);
+                    crearProgramacion(tipoBingo, horario, tablasVentas, programaciones, sedes, dateBegin);
+                    countHorariosDJ = 0;
+                }
+                //Viernes, Sabado y Domingo, 2 Horas
+                //SUPER BINGOTE
+                countHorariosVSD++;
+                if (countHorariosVSD == 4) {
+                    tipoBingo = tipoBingoController.findTipoBingos(4);
+                    crearProgramacion(tipoBingo, horario, tablasVentas, programaciones, sedes, dateBegin);
+                    countHorariosVSD = 0;
+                }
+                //Final 
+                //BINGAZO
+                counthorariosFinal++;
+                if (counthorariosFinal == (listHorarios.size() - 1)) {
+                    tipoBingo = tipoBingoController.findTipoBingos(5);
+                    crearProgramacion(tipoBingo, horario, tablasVentas, programaciones, sedes, dateBegin);
+                    counthorariosFinal = 0;
+                }
+                //BINGAMES
+                Date dateEnd = new Date((long) dateBegin.getTime() + (86400000));
+                int monthNewEnd = dateEnd.getMonth();
+                if (monthOld != monthNewEnd) {
+                    tipoBingo = tipoBingoController.findTipoBingos(6);
+                    if (dateUtil.isHoliday(dateEnd.getMonth(), dateEnd.getDay())) {
+                        crearProgramacion(tipoBingo, horario, tablasVentas, programaciones, sedes, new Date((long) dateBegin.getTime() - (86400000)));
+                    }
+                }
+            }
+        }
+    }
+
+    public void crearProgramacion(TipoBingos tipoBingo,
+            Horarios horario,
+            Integer countTable,
+            ProgramacionesJpaController programaciones,
+            Sedes sede,
+            Date fecha) {
+        crearProgramacion(tipoBingo, sede, horario, countTable, programaciones, fecha, 1);
+    }
     
-    
+    public void crearProgramacion(TipoBingos tipoBingo,
+            Horarios horario,
+            Integer countTable,
+            ProgramacionesJpaController programaciones,
+            Sedes sede,
+            Date fecha,
+            Integer estado) {
+        crearProgramacion(tipoBingo, sede, horario, countTable, programaciones, fecha, estado);
+    }
+
+    public void crearProgramacion(TipoBingos tipoBingo,
+            Sedes sede,
+            Horarios horario,
+            Integer countTable,
+            ProgramacionesJpaController programaciones,
+            Date fecha,
+            Integer estado) {
+
+        Programaciones programacion = new Programaciones();
+        programacion.setIdSede(sede);
+        programacion.setIdHorario(horario);
+        programacion.setIdTipoBingo(tipoBingo);
+        programacion.setCantidadGanadores(String.valueOf(0));
+        programacion.setCantidadPremio(tipoBingo.getValorPremio());
+        programacion.setCantidadTablas(countTable);
+        programacion.setFecha(fecha);
+        programacion.setEstado(estado);
+        programaciones.create(programacion);
+
+    }
 }
